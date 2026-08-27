284f1ec5a75eb6b1d7b1ecf9639a554800240203
Ekram-Miftah <ekrammiftah013@gmail.com>
2026-08-27 22:37:25 +0300

    feat: add CartProvider and CartSidebar components for cart management

---
+++ client/src/components/CartSidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartSidebar = () => {
+  return (
+    <>
+      {/*Overlay*/}
+      <div className="fixed inset-0 bg-black/40 z-50 transition-opacity" />
+    </>
+  );
+};
+
+export default CartSidebar;
